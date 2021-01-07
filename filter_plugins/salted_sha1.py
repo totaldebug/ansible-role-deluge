de53c9cec40504af06cbe3681ea8e7be36f9644e
marksie1988 <marksie1988@users.noreply.github.com>
2021-01-07 13:12:27 +0000

    fix: filter salt

--- filter_plugins/salted_sha1.py
+++ filter_plugins/salted_sha1.py
@@ -2,25 +2,24 @@ from ansible import errors
 
 import hashlib
 import secrets
-
-def salted_sha1(raw_password, salt):
-        """
-        Returns a string of the hexdigest of the given plaintext password and salt
-        using the sha1 algorithm.
-        """
-        return hashlib.sha1(salt + raw_password).hexdigest()
-
-def random_salt(salt_length):
-    """
-    returns a random hex token to use as a salt
-    """
-    salt = secrets.token_hex(salt_length)
-    return salt.encode('utf-8')
-
 class FilterModule(object):
     ''' A filter to salt sha1-encrypted passwords. '''
     def filters(self):
         return {
-            'random_salt': random_salt,
-            'salted_sha1': salted_sha1
+            'random_salt': self.random_salt,
+            'salted_sha1': self.salted_sha1
         }
+
+    def random_salt(self, salt_length):
+        """
+        returns a random hex token to use as a salt
+        """
+        salt = secrets.token_hex(salt_length)
+        return salt.encode('utf-8')
+
+    def salted_sha1(self, raw_password, salt):
+            """
+            Returns a string of the hexdigest of the given plaintext password and salt
+            using the sha1 algorithm.
+            """
+            return hashlib.sha1(salt + raw_password).hexdigest()
